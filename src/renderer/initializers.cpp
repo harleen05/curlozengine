1b6d7d4abe383c57aa664262f2f53779b07621e0
curloz123 <curloz@tutamail.com>
2026-06-10 19:48:56 +0530

    feature: added entity component system, refactored some code from renderer

--- src/renderer/initializers.cpp
+++ src/renderer/initializers.cpp
@@ -24,7 +24,7 @@ namespace clz::renderer
 		if (auto surfacerResult = createSurface(); !surfacerResult)
 			return std::unexpected(surfacerResult.error());
 
-		if (auto gpuResult = selectPhysicalDevice(); gpuResult)
+		if (auto gpuResult = selectPhysicalDevice(); !gpuResult)
 			return std::unexpected(gpuResult.error());
 
 		if (auto logicalDeviceResult = createLogicalDevice(); !logicalDeviceResult)
@@ -56,13 +56,13 @@ namespace clz::renderer
 
 	std::expected<void, std::string> initFrameContext()
 	{
-		if (auto commandPoolResult = createCommandPool(); commandPoolResult)
+		if (auto commandPoolResult = createCommandPool(); !commandPoolResult)
 			return std::unexpected(commandPoolResult.error());
 
-		if (auto commandBufferResult = createCommandBuffer(); commandBufferResult)
+		if (auto commandBufferResult = createCommandBuffer(); !commandBufferResult)
 			return std::unexpected(commandBufferResult.error());
 
-		if (auto syncObjectResult = createSyncObjects(); syncObjectResult)
+		if (auto syncObjectResult = createSyncObjects(); !syncObjectResult)
 			return std::unexpected(syncObjectResult.error());
 
 		clz::log::debug("initialized frame context successfully");
